971095cd6485724a24a9094108aa16a226f72800
vutheanh1zz <256341286+tranthinam162022-svg@users.noreply.github.com>
2026-02-05 06:46:39 +0700

    Create create_and_check_btc_address.py

---
+++ create_and_check_btc_address.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run([
+	"python", "derive_addresses.py",
+	"--mnemonic", "wool hospital bounce mushroom minor job acid brass rug hockey breeze plastic usual ridge strategy elder throw pepper awesome early abuse island feed guess",
+	"--json"
+])
